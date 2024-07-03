efb1262520e1bd7501d6598cdb4f4e48abac67f8
Suchir V <125119931+suchir31@users.noreply.github.com>
2024-07-04 02:17:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: [],
+        origin: [https://soci-api.vercel.app/],
         methods: ["POST", "GET"],
         credentials: true
     }
